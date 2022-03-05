f2bd0cbda89e7a151e42bdddc5a4413172cff066
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-05 22:02:36 +0000

    function-1-5.cpp

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,17 +1,15 @@
-#include <iostream> 
+#include <iostream>
 using namespace std; 
 
-int count_even(int number){
-   int counteven = 0; 
-   
-    if(number < 1){
-               return 0; 
-       } 
-       else{
-       for (int i=0;1<i<number; i++){
-       if(i % 2 == 0){ 
-            counteven ++; 
-   }
-   }}
-   return counteven; 
-}
+int count_even(int number); 
+
+ int main(){
+  int number; 
+  cout << "Enter the Numbers:"; 
+  cin >> number;
+  
+  int counteven = count_even(number); 
+  cout << "To count the even number:" << counteven << endl; 
+  
+  return 0; 
+ }
