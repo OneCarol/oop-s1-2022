1b67af557d16ce228ac6a817effcfe70f524bd56
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-05 22:17:24 +0000

    main-1-5.cpp

--- main-1-5.cpp
+++ main-1-5.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 using namespace std; 
 
-int count_even(int number){
- int counteven = 0; 
- int array[number];
- 
-  for(int i=0;i<number;i++){
-      if (number<1){
-     return 0; 
+int count_even(int number); 
+
+ int main(){
+  int number; 
+  cout << "Enter the Numbers:"; 
+  cin >> number;
+  
+  int counteven = count_even(number); 
+  cout << "To count the even number:" << counteven << endl; 
+  
+  return 0; 
  }
-     else {
-        
-         if (array[i]%2 == 0){
-             counteven ++; 
-         }
-     }
-     }
- return counteven; 
-}
