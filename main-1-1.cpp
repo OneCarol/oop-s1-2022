f9a5baf612a36b55d9ca1f525be7acdf76b0dfa9
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-05 20:29:15 +0000

    function-1-1.cpp

---
+++ main-1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+
+int sum_array(int array[], int n); 
+
+int main() {
+    int array[ ] = {1,2,3,4,5}; 
+    int n=5; 
+    cout << "The sum is:" << sum_array(array,n) << endl; 
+
+}
+    
