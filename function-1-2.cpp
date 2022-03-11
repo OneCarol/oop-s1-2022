1eb86634fa00dfc8df3fb3a2e1ba5d1fe2d97dff
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-11 05:41:53 +0000

    .

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -2,22 +2,24 @@
 using namespace std;
 
 int identity(int array[10][10]){
+    int number = 0;
+
     int i;
     int j;
 
     for(i=0;i<10;i++){
         for(j=0;j<10;j++){
 
-        if(i==j && array[i][j] == 1 || i!=j&& array[i][j]==0){
-                return 1;
+        if(i==j && array[i][j] == 1 || i!=j&&array[i][j]==0){
+                number ++;
             }
-              else {
 
-                return 0;
-
-            }
         }
 
+    }
+
+    if(number == 100){
+        return 1;
     }
     return 0;
 }
--- function-1-5.cpp
+++ function-1-5.cpp
@@ -17,7 +17,7 @@ void print_summed_matrices(int array1[3][3], int array2[3][3]){
 
      for(i=0;i<3;i++){
         for(j=0;j<3;j++){
-            if(i==0&&i==1&&j==3){
+            if(i==0&&i==1&&j==2){
               cout<<sumatrix[i][j];}
           else {
 
