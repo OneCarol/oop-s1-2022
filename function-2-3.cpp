15d38909fa1ed9910250e36466ec1396e55e7eb3
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-17 00:34:45 +0000

    .

---
+++ function-2-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int sum_if_a_palindrome(int integers[]0, int length){
+    
+}
+
+bool is_a_palindrome(int integers[], int length){
+    
+}
+
+int sum_elements(int integers[], int length){
+    
+}
