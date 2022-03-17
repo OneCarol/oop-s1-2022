e698bc346cd612e10744bd0e5bd27804ff28582b
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-17 01:22:35 +0000

    .

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-int sum_if_a_palindrome(int integers[], int length); 
-bool is_a_palindrome(int integers[], int length); 
-int sum_elements(int integers[], int length); 
+int sum_if_a_palindrome(int integers[], int length);
+bool is_a_palindrome(int integers[], int length);
+int sum_elements(int integers[], int length);
 
 int main(){
-    return 0; 
+    int integers[] = {1,2,2,1};
+    int length = 5;
+
+    cout << "Whether it is a palindrome array:" << is_a_palindrome(integers,length) << endl;
+
+    cout << "the sum of integer array:" << sum_if_a_palindrome(integers, length) << endl;
+    cout << sum_if_a_palindrome(integers,length) << endl;
+    return 0;
 }
