ed8f4b864a0e5d9fa2b77145ee4224ae484a3b50
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-24 01:01:51 +0000

    .

---
+++ exercise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    char first[3][2] = {"A", "Q", "A"};
+
+    for(int i=0;i<3;i++){
+        cout << first[i] << endl; 
+        
+    }
+
+        return 0;
+}
---
+++ function-1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void copy_2d_strings(std::string first[][2], std::string second[][2], int n){
+
+    for(int i=0;i<3;i++){
+
+        first[i][2] == second[i][2];
+
+        cout << second[i] << endl;
+    }
+
+}
