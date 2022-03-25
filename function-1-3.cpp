c53ac1e33a7087c8cff01f39794bf4c76ac40c9a
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-25 07:09:51 +0000

    .

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -6,11 +6,7 @@ void cpyia(int old_array[], int new_array[], int length){
 
     for(int i=0;i<5;i++){
 
-        new_array[i] = old_array[i];
-
-        cout << *new_array << " ";
+        *(new_array+i) = *(old_array+i);
 
     }
-
-    cout << endl;
 }
