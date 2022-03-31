e19cd91db4ee643120eb09c685ebfaeb36b90ef5
OneCarol <a1829917@student.adelaide.edu.au>
2022-03-31 01:13:04 +0000

    .

---
+++ exercise1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+
+int lookup_fishing_fine(int over){
+    
+    int fish_over = 0; 
+    
+    for(fish_over=)
+    
+}    
---
+++ function-4-1.cpp
@@ -0,0 +1,9 @@
+#include "function-4-1.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Buscount{
+    
+    
+}
